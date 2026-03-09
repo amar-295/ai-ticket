445291ad5f09e02e91cfe48ce50ea0776e1c789c
amar-295 <612amar@gmail.com>
2026-03-09 20:25:37 +0530

    learned to builed pipeline

---
+++ ai-ticket-assistant/inngest/functions/on-signup.js
@@ -0,0 +1,38 @@
+import { inngest } from "../client";
+import { User } from "../../models/user.model.js"
+import { NonRetriableError } from "inngest";
+import { sendMail } from "../../utils/mailer";
+
+export const onUserSignup = inngest.createFunction(
+  { id: "on-user-signup", retries: 2 },
+  { event: "user/signup" },
+  async ({event, step}) => {
+    try {
+        const { email } = event.data
+        const user = await step.run("get-user-email", async () => {
+           const userObject = await User.findOne({ email })
+
+           if (!userObject) {
+            throw new NonRetriableError("User no longer exists in our database")
+           }
+
+           return userObject
+        })
+
+        await step.run("send-welcome-email", async () => {
+            const subject = `Welcome to the app`
+            const message = `Hi,
+            \n\n
+            Thanks for sign up. We're glad to have you onboard!
+            `
+
+            await sendMail(user.email, subject, message)
+        })
+
+        return { success: true }
+    } catch (error) {
+        console.error("❌ Error running step", error.message)
+        return { success:false }
+    }
+  }
+);
