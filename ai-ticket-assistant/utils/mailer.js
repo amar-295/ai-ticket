1829d28dec3840aa423ede240f790ebc57dc79cb
amar-295 <612amar@gmail.com>
2026-03-09 19:27:08 +0530

    email utility done

--- ai-ticket-assistant/utils/mailer.js
+++ ai-ticket-assistant/utils/mailer.js
@@ -1,26 +1,30 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
-// Create a transporter using Ethereal test credentials.
-// For production, replace with your actual SMTP server details.
-const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 587,
-  secure: false, // Use true for port 465, false for port 587
-  auth: {
-    user: "maddison53@ethereal.email",
-    pass: "jn7jnAPss4f63QBp6D",
-  },
-});
+export const sendMail = async (to, subject, text) => {
+  try {
+    // Create a transporter using Ethereal test credentials.
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAILTRAP_SMTP_HOST,
+      port: process.env.MAILTRAP_SMTP_PORT,
+      secure: false, // Use true for port 465, false for port 587
+      auth: {
+        user: process.env.MAILTRAP_SMTP_USER,
+        pass: process.env.MAILTRAP_SMTP_PASS,
+      },
+    });
 
-// Send an email using async/await
-(async () => {
-  const info = await transporter.sendMail({
-    from: '"Maddison Foo Koch" <maddison53@ethereal.email>',
-    to: "bar@example.com, baz@example.com",
-    subject: "Hello ✔",
-    text: "Hello world?", // Plain-text version of the message
-    html: "<b>Hello world?</b>", // HTML version of the message
+    const info = await transporter.sendMail({
+    from: "no-reply-from-amarnath",
+    to,
+    subject,
+    text, // Plain-text version of the message
+    // html: "<b>Hello world?</b>", // HTML version of the message
   });
 
   console.log("Message sent:", info.messageId);
-})();
+  return info
+  } catch (error) {
+    console.log("❌ Mail error", error.message)
+    throw error
+  }
+};
